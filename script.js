795fb4caae701c0692a53c5c28027eecd6be7211
Christopher Pawliuk <mr.cpawliuk@gmail.com>
2024-07-17 02:30:09 -0600

    Code clean up a bit, and added a scroll to the top on first load just to avoid any problems where the div might get hidden so at least the user can see the startup options.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Substance Asset Page Auto Select and Downloader
 // @website      https://github.com/cpawliuk/Substance-Asset-Page-Auto-Select-And-Downloader
-// @version      1.0
-// @description  Simple script to auto select the GLB download option
+// @version      1.1
+// @description  Simple script to auto select the GLB download option and auto download the file.
 // @author       Christopher Pawliuk
 // @match        *://substance3d.adobe.com/assets*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=adobe.com
@@ -19,20 +19,13 @@ window.ToggleDownload = () => {
     document.getElementById("toggle-button").textContent = window.downloadFile ? "Off" : "On";
 };
 
-addEventListener("load", setTimeout(SelectGLB, 3000)); // Set the timeout higher if needed due to the client side delay in loading the components.
+addEventListener("load", setTimeout(RunScript, 3000)); // Set the timeout higher if needed due to the client side delay in loading the components.
 
 function DownloadFile() {
     document.querySelector(".spectrum-SplitButton-action").click();
 }
 
 function SelectGLB() {
-    const newElement = document.createElement("div");
-    newElement.id = "script-ui";
-    newElement.style.color = "white";
-    newElement.style.backgroundColor = "black";
-    newElement.innerHTML = `<b>Running GLS Auto Select Script - Auto Download is: <span id=auto-dl-text>${window.downloadFile ? "Enabled" : "Disabled"}</span> -> Toggle: </b><button id="toggle-button" onClick="window.ToggleDownload()">${window.downloadFile ? "Off" : "On"}</button>`;
-    document.getElementById("root").prepend(newElement);
-
     try {
         document.querySelector(".spectrum-SplitButton-trigger").click();
         document.querySelector(".spectrum-Menu-item").nextElementSibling.click();
@@ -41,16 +34,26 @@ function SelectGLB() {
     } catch (e) {
         console.log(`Script Error: ${e}`);
     }
+}
+
+function RunScript() {
+    const newElement = document.createElement("div");
+    newElement.id = "script-ui";
+    newElement.style.color = "white";
+    newElement.style.backgroundColor = "black";
+    newElement.innerHTML = `<b>Running GLS Auto Select Script - Auto Download is: <span id=auto-dl-text>${window.downloadFile ? "Enabled" : "Disabled"}</span> -> Toggle: </b><button id="toggle-button" onClick="window.ToggleDownload()">${window.downloadFile ? "Off" : "On"}</button>`;
+    document.getElementById("root").prepend(newElement);
+
+    window.scrollTo(0, 0);
+
+    SelectGLB();
 
     // Script will continue to call the function every 3000 ms due to React being used for the site.
     // Only if GLB isn't already selected.
     setInterval(() => {
         try {
             if (document.querySelector(".spectrum-SplitButton-action").textContent !== "Download (GLB)") {
-                document.querySelector(".spectrum-SplitButton-trigger").click();
-                document.querySelector(".spectrum-Menu-item").nextElementSibling.click();
-
-                window.downloadFile ? DownloadFile() : null;
+                SelectGLB();
             }
         } catch (e) {
             console.log(`Script Error: ${e}`);
