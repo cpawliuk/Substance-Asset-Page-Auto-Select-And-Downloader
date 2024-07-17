e326aeb2906a08d520f18c211d6da253142c6e2c
Christopher Pawliuk <mr.cpawliuk@gmail.com>
2024-07-17 01:44:54 -0600

    Added Auto Download feature, plus a UI.

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Edit the setTimeout value to be higher if needed.
 
 Script will continue to call the function every 3000 ms due to React being used for the site only if the GLB option hasn't been selected.
 
-Set downloadFile to true if you want to auto download files on page load after the GLB file has been selected.
+Set downloadFile to true if you want to auto download files on page load after the GLB file has been selected by default.
+
+UI will show up at the top of the page which will state the Auto Download condition, and allow the user to toggle it off or on.
--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // @version      2024-07-17
 // @description  Simple script to auto select the GLB download option
 // @author       Christopher Pawliuk
-// @match        *://substance3d.adobe.com/assets/*
+// @match        *://substance3d.adobe.com/assets*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=adobe.com
 // @grant        none
 // ==/UserScript==
@@ -12,27 +12,48 @@
 window.setTimeout = window.setTimeout.bind(window);
 window.setInterval = window.setInterval.bind(window);
 
-const downloadFile = false; // Set to True if you want to auto download.
+window.downloadFile = false; // Set to True if you want to auto download.
+window.ToggleDownload = () => {
+    window.downloadFile = !window.downloadFile;
+    document.getElementById("auto-dl-text").textContent = window.downloadFile ? "Enabled" : "Disabled";
+    document.getElementById("toggle-button").textContent = window.downloadFile ? "Off" : "On";
+};
 
 addEventListener("load", setTimeout(SelectGLB, 3000)); // Set the timeout higher if needed due to the client side delay in loading the components.
 
 function DownloadFile() {
-    document.getElementsByClassName("spectrum-SplitButton-action")[0].click();
+    document.querySelector(".spectrum-SplitButton-action").click();
 }
 
 function SelectGLB() {
-    document.getElementsByClassName("spectrum-SplitButton-trigger")[0].click();
-    document.getElementsByClassName("spectrum-Menu-item")[1].click();
-    downloadFile ? DownloadFile() : null;
+    const newElement = document.createElement("div");
+    newElement.id = "script-ui";
+    newElement.style.color = "white";
+    newElement.style.backgroundColor = "black";
+    newElement.innerHTML = `<b>Running GLS Auto Select Script - Auto Download is: <span id=auto-dl-text>${window.downloadFile ? "Enabled" : "Disabled"}</span> -> Toggle: </b><button id="toggle-button" onClick="window.ToggleDownload()">${window.downloadFile ? "Off" : "On"}</button>`;
+    document.getElementById("root").prepend(newElement);
+
+    try {
+        document.querySelector(".spectrum-SplitButton-trigger").click();
+        document.querySelector(".spectrum-Menu-item").nextElementSibling.click();
+
+        window.downloadFile ? DownloadFile() : null;
+    } catch (e) {
+        console.log(`Script Error: ${e}`);
+    }
 
     // Script will continue to call the function every 3000 ms due to React being used for the site.
     // Only if GLB isn't already selected.
     setInterval(() => {
-        if (document.getElementsByClassName("spectrum-SplitButton-action")[0].firstChild.textContent !== "Download (GLB)") {
-            document.getElementsByClassName("spectrum-SplitButton-trigger")[0].click();
-            document.getElementsByClassName("spectrum-Menu-item")[1].click();
+        try {
+            if (document.querySelector(".spectrum-SplitButton-action").textContent !== "Download (GLB)") {
+                document.querySelector(".spectrum-SplitButton-trigger").click();
+                document.querySelector(".spectrum-Menu-item").nextElementSibling.click();
 
-            downloadFile ? DownloadFile() : null;
+                window.downloadFile ? DownloadFile() : null;
+            }
+        } catch (e) {
+            console.log(`Script Error: ${e}`);
         }
     }, 3000);
 }
